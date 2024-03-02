ef99d3d52002206f465884648d3a9ece55c63569
Aaliya-17 <aaliyashenaaz@gmailcom>
2024-03-02 19:09:24 +0530

    done

--- multivendor_api/product/models.py
+++ multivendor_api/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    selling_price = models.FloatField()
+    discount_price = models.FloatField()
+    description = models.CharField(max_length=255)  
+    product_image = models.ImageField(upload_to='product_images/')  
+    category = models.CharField(max_length=50)  
