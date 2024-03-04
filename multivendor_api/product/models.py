86606f094a4e6583eb292ec4a498e0b8fbc3fad7
md abbas ali <abbasaliashrafi1@gmail.com>
2024-03-04 17:54:31 +0530

    created product

--- multivendor_api/product/models.py
+++ multivendor_api/product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+# Product model :
+#     title = CharField()
+#     selling_price = FloatField()
+#     discount_price = FloatField()
+#     description = CharField
+#     product_image = ImageField()
+#     category = CharField()
+
+class Product(models.Model):
+    title = models.CharField(max_length = 100)
+    selling_price = models.FloatField()
+    actual_price = models.FloatField()
+    description = models.CharField(max_length = 500) 
+    product_image = models.ImageField(upload_to= "product_image")
+    category = models.CharField(max_length=100)
+    sub_category = models.CharField(max_length=100)
+    
