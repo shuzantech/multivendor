63c8fce7db6ab0ed13b0d95375431c33cfb2af64
Aaliya-17 <aaliyashenaaz@gmailcom>
2024-03-05 19:59:17 +0530

    done

--- multivendor_api/product/views.py
+++ multivendor_api/product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
