22c247d5fedb747f4117bdd1eb71b4e8e8fa43b5
SimranSultana <simran07sultana@gmail.com>
2024-03-11 18:29:58 +0530

    payment updates

--- multivendor_api/payment/models.py
+++ multivendor_api/payment/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from account.models import User
 # Create your models here.
 class Payment(models.Model):
-    user =models.ForeignKey(User, on_delete =models.CASCADE )# need to add "related_name here"
+    user =models.ForeignKey(User, related_name="users_payment",on_delete =models.CASCADE )# need to add "related_name here"
     amount = models.FloatField()
     transaction_id =models.CharField(max_length=15)
     paid_status =models.BooleanField(default=False)
